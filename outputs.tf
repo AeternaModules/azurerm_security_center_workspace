eccf031c0c5f20706a8dab0cbd78060f6d3b59ff
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:20:08 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,13 +1,9 @@
-output "security_center_workspaces" {
-  description = "All security_center_workspace resources"
-  value       = azurerm_security_center_workspace.security_center_workspaces
-}
 output "security_center_workspaces_scope" {
-  description = "List of scope values across all security_center_workspaces"
-  value       = [for k, v in azurerm_security_center_workspace.security_center_workspaces : v.scope]
+  description = "Map of scope values across all security_center_workspaces, keyed the same as var.security_center_workspaces"
+  value       = { for k, v in azurerm_security_center_workspace.security_center_workspaces : k => v.scope }
 }
 output "security_center_workspaces_workspace_id" {
-  description = "List of workspace_id values across all security_center_workspaces"
-  value       = [for k, v in azurerm_security_center_workspace.security_center_workspaces : v.workspace_id]
+  description = "Map of workspace_id values across all security_center_workspaces, keyed the same as var.security_center_workspaces"
+  value       = { for k, v in azurerm_security_center_workspace.security_center_workspaces : k => v.workspace_id }
 }
 
