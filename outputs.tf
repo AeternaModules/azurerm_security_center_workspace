3f781cf0e3b581db9de2fa7dcbefe8b9977241f4
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:04:41 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "security_center_workspaces_id" {
+  description = "Map of id values across all security_center_workspaces, keyed the same as var.security_center_workspaces"
+  value       = { for k, v in azurerm_security_center_workspace.security_center_workspaces : k => v.id }
+}
 output "security_center_workspaces_scope" {
   description = "Map of scope values across all security_center_workspaces, keyed the same as var.security_center_workspaces"
   value       = { for k, v in azurerm_security_center_workspace.security_center_workspaces : k => v.scope }
